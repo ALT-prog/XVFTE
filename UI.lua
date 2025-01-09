538ebcd1e3247a328fb71e9d7dadca81500e0762
ALT-prog <79774760+ALT-prog@users.noreply.github.com>
2025-01-09 14:11:07 -0600

    Update UI.lua

--- UI.lua
+++ UI.lua
@@ -808,7 +808,11 @@ Windows.Position = UDim2.new(0, 20, 0, 20)
 Windows.Size = UDim2.new(1, 20, 1, -20)
 
 --[[ Script ]]--
---script.Parent = imgui
+local actual_script = script
+local script = {
+	Parent = imgui
+}
+script.Parent = imgui
 
 local UIS = game:GetService("UserInputService")
 local TweenService = game:GetService("TweenService")
